be90a9bb9ed4397d03a1c000b23ca69ab5cb35cb
Jung-Minji <mjjunghp@naver.com>
2019-12-15 19:43:36 +0900

    add scoreDB processing

--- SWProject.py
+++ SWProject.py
@@ -2,10 +2,19 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from InputDialog import InputDialog
+import pickle
+import os
+
 
 class Main(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.class1_SW_score = []
+        self.class1_LA_score = []
+        self.class2_SW_score = []
+        self.class2_LA_score = []
+        self.setData()
         self.initUI()
         self.drawGraph()
 
@@ -25,10 +34,10 @@ class Main(QWidget):
         self.lblClass.setFont(font)
         self.comboClass = QComboBox()
         self.comboClass.setFixedSize(80, 30)
-        self.comboSubject.addItems(['소프트웨어프로젝트2', '창업연계공학설계입문', '객체지향프로그램', '선형대수'])
-        self.comboClass.addItems(['class1', 'class2', 'class3'])
-        self.comboClass.currentTextChanged.connect(self.comboClicked)
-        self.comboSubject.currentTextChanged.connect(self.comboClicked)
+        self.comboSubject.addItems(['소프트웨어프로젝트2', '선형대수'])
+        self.comboClass.addItems(['class1', 'class2'])
+        # self.comboClass.currentTextChanged.connect(self.comboClicked)
+        # self.comboSubject.currentTextChanged.connect(self.comboClicked)
 
         self.btnInputScore = QPushButton("점수 입력하기")
         self.btnInputScore.clicked.connect(self.btnInputScoreClicked)
@@ -43,10 +52,10 @@ class Main(QWidget):
         self.btnReset.setFont(font)
         self.btnReset.setFixedSize(200, 40)
 
-        self.checkClass = QCheckBox("분반 별 그래프 보기")
-        font = self.checkClass.font()
-        font.setPointSize(font.pointSize() + 3)
-        self.checkClass.setFont(font)
+        # self.checkClass = QCheckBox("분반 별 그래프 보기")
+        # font = self.checkClass.font()
+        # font.setPointSize(font.pointSize() + 3)
+        # self.checkClass.setFont(font)
         self.checkAll =  QCheckBox("모든 분반 그래프 보기")
         font = self.checkAll.font()
         font.setPointSize(font.pointSize() + 3)
@@ -65,8 +74,8 @@ class Main(QWidget):
         funcLayout.addSpacing(10)
         funcLayout.addWidget(self.btnReset)
         funcLayout.addSpacing(60)
-        funcLayout.addWidget(self.checkClass)
-        funcLayout.addSpacing(10)
+        # funcLayout.addWidget(self.checkClass)
+        # funcLayout.addSpacing(10)
         funcLayout.addWidget(self.checkAll)
         funcLayout.setContentsMargins(20, 50, 0, 50)    # layout margins(left, top, right, bottom)
 
@@ -119,16 +128,101 @@ class Main(QWidget):
         self.setLayout(mainLayout)
 
 
-    # clicked btnInputScore
+    # Subject comboBox의 key값 리턴
+    def getSubKey(self):
+        return self.comboSubject.currentText()
+
+    # Class comboBox의 key값 리턴
+    def getClassKey(self):
+        return self.comboClass.currentText()
+
+    # clicked btnInputScore -> dialog
     def btnInputScoreClicked(self):
-        file = open("scoreDB.txt", 'w')
+        self.data = []
+        self.setScore()
+        self.dlg = InputDialog(self.data)   # 매개변수 self.data : dialog에 입력했던 점수를 불러오기 위한 장치
+        self.dlg.exec_()
+        self.dumpData()
+        self.setData()
 
 
+    # 다이얼로그 창에서 받아온 점수리스트를 comboBox의 키 값에 따라 각각의 리스트에 점수 저장 & 텍스트파일에 값 저장
+    def dumpData(self):
+        n = self.dlg.btnOkClicked()     # dialog에 입력한 점수 리스트
+        if self.getSubKey() == "소프트웨어프로젝트2" and self.getClassKey() == "class1":
+            file = open('class1_SW_scoreDB.txt', 'wb')
+            pickle.dump(n, file)
+
+        elif self.getSubKey() == "선형대수" and self.getClassKey() == "class1":
+            file = open('class1_LA_scoreDB.txt', 'wb')
+            pickle.dump(n, file)
+
+        elif self.getSubKey() == "소프트웨어프로젝트2" and self.getClassKey() == "class2":
+            file = open('class2_SW_scoreDB.txt', 'wb')
+            pickle.dump(n, file)
+
+        elif self.getSubKey() == "선형대수" and self.getClassKey() == "class2":
+            file = open('class2_LA_scoreDB.txt', 'wb')
+            pickle.dump(n, file)
 
 
-    def comboClicked(self):
-        self.txtGraphTitle = QLabel("< " + self.comboSubject.currentText() + " > - " +
-                                self.comboClass.currentText() + " graph")
+    # 텍스트파일에 저장되어있는 점수 불러오기
+    def setData(self):
+        if os.path.getsize('class1_SW_scoreDB.txt') > 0:
+            file = open('class1_SW_scoreDB.txt', 'rb')
+            self.class1_SW_score = pickle.load(file)
+        else:
+            pass
+
+        if os.path.getsize('class1_LA_scoreDB.txt') > 0:
+            file = open('class1_LA_scoreDB.txt', 'rb')
+            self.class1_LA_score = pickle.load(file)
+        else:
+            pass
+
+        if os.path.getsize('class2_SW_scoreDB.txt') > 0:
+            file = open('class2_SW_scoreDB.txt', 'rb')
+            self.class2_SW_score = pickle.load(file)
+        else:
+            pass
+
+
+        if os.path.getsize('class2_LA_scoreDB.txt') > 0:
+            file = open('class2_LA_scoreDB.txt', 'rb')
+            self.class2_LA_score = pickle.load(file)
+        else:
+            pass
+
+    # 각각의 파일에서 점수를 가져와서 다이얼로그 lineEdit에 점수 저장
+    def setScore(self):
+        if self.getSubKey() == "소프트웨어프로젝트2" and self.getClassKey() == "class1":
+            if os.path.getsize('class1_SW_scoreDB.txt') > 0:
+                file = open('class1_SW_scoreDB.txt', 'rb')
+                self.data =  pickle.load(file)
+            else:
+                self.data = []
+
+        elif self.getSubKey() == "선형대수" and self.getClassKey() == "class1":
+            if os.path.getsize('class1_LA_scoreDB.txt') > 0:
+                file = open('class1_LA_scoreDB.txt', 'rb')
+                self.data =  pickle.load(file)
+            else:
+                self.data = []
+
+        elif self.getSubKey() == "소프트웨어프로젝트2" and self.getClassKey() == "class2":
+            if os.path.getsize('class2_SW_scoreDB.txt') > 0:
+                file = open('class2_SW_scoreDB.txt', 'rb')
+                self.data =  pickle.load(file)
+            else:
+                self.data = []
+
+        elif self.getSubKey() == "선형대수" and self.getClassKey() == "class2":
+            if os.path.getsize('class2_LA_scoreDB.txt') > 0:
+                file = open('class2_LA_scoreDB.txt', 'rb')
+                self.data =  pickle.load(file)
+            else:
+                self.data = []
+
 
 
     def drawGraph(self):
@@ -145,7 +239,7 @@ class Main(QWidget):
         ax.set_yticks(self.people)
         ax.plot(x, y, label="class1", marker='o')
         ax.plot(x1, y1, label="class2", marker='o')
-        ax.legend()     # 범례 표시
+        ax.legend()  # 범례 표시
 
         self.canvas.draw()
 
